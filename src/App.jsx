d765bdf843d306a48e7ee7aa721772a3af64b9cd
Emilio Pino <pinoen@yahoo.com.ar>
2023-08-22 11:44:41 -0300

    se envuelve con cart provider

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Products from "./components/Products"
 import { products } from './mocks/products.json'
 import useFilters from "./hooks/useFilters"
 import { Footer } from "./components/Footer"
+import Cart from "./components/Cart"
+import { CartProvider } from "./contexts/CartContext"
 
 function App() {
   const { filterList } = useFilters()
@@ -10,11 +12,12 @@ function App() {
   const filteredProducts = filterList(products)
 
   return (
-    <>
+    <CartProvider>
       <Header list={filteredProducts} />
+      <Cart />
       <Products list={filteredProducts} />
       <Footer />
-    </>
+    </CartProvider>
   )
 }
 
