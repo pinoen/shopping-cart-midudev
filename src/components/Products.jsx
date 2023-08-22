3b8d460420b28c7544925822f75c49e7ecbc8e1a
Emilio Pino <pinoen@yahoo.com.ar>
2023-08-22 11:46:42 -0300

    se usa useCart para renderizado de productos

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,22 +1,34 @@
 /* eslint-disable react/prop-types */
 import './Products.css'
-import { AddToCartIcon } from './Icons'
+import { AddToCartIcon, RemoveFromCartIcon } from './Icons'
+import useCart from '../hooks/useCart'
 
 const Products = ({ list }) => {
+  const { addToCart, cart, removeFromCart } = useCart()
+
+  const checkProductInCart = (product) => {
+    return cart.some(item => item.id === product.id)
+  }
+
   return (
     <main className='products'>
       <ul>
-        {list.slice(0, 10).map(product => (
-          <li key={product.id}>
-            <img src={product.thumbnail} alt={product.title} />
-            <div>
-              <strong>{product.title}</strong><span> - ${product.price}</span>
-            </div>
-            <div>
-              <button><AddToCartIcon /></button>
-            </div>
-          </li>
-        ))}
+        {list.slice(0, 10).map(product => {
+
+          const isProductInCart = checkProductInCart(product)
+
+          return (
+            <li key={product.id}>
+              <img src={product.thumbnail} alt={product.title} />
+              <div>
+                <strong>{product.title}</strong><span> - ${product.price}</span>
+              </div>
+              <div>
+                <button style={{ backgroundColor: isProductInCart ? 'red' : 'steelblue' }} onClick={isProductInCart ? () => removeFromCart(product) : () => addToCart(product)}>{isProductInCart ? <RemoveFromCartIcon /> : <AddToCartIcon />}</button>
+              </div>
+            </li>
+          )
+        })}
       </ul>
     </main>
   )
