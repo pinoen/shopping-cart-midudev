d7a2cd496fa76305e3e2a3e18cbf127bce6bf215
Emilio Pino <pinoen@yahoo.com.ar>
2023-08-22 11:44:18 -0300

    se retira strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { FilterProvider } from './contexts/FilterContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <FilterProvider>
-      <App />
-    </FilterProvider>
-  </React.StrictMode>,
+  <FilterProvider>
+    <App />
+  </FilterProvider>
 )
