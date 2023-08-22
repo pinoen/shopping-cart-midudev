075f0dc44a1def91619601693b57d1a9f044aa07
Emilio Pino <pinoen@yahoo.com.ar>
2023-08-21 21:00:14 -0300

    se agrega estado para los filtros a aplicar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react"
+import Header from "./components/Header"
 import Products from "./components/Products"
 import list from './mocks/products.json'
 
 function App() {
+  const [filters, setFilters] = useState({
+    maxPrice: 2000,
+    category: 'All'
+  })
+
+  const filterList = (products) => {
+    return products.filter(product => product.price <= filters.maxPrice && (filters.category === 'All' || product.category === filters.category))
+  }
+
+  const filteredProducts = filterList(list.products)
+
   return (
-    <div>
-      <Products list={list} />
-    </div>
+    <>
+      <Header list={filteredProducts} setFilters={setFilters} />
+      <Products list={filteredProducts} />
+    </>
   )
 }
 
